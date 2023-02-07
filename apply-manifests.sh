f43590f688a0c97e20b6094e6423cf20598b91b8
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2023-02-07 09:44:19 -0300

    refactor(scripts): apply manifests dir as parameter of script

--- apply-manifests.sh
+++ apply-manifests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-directory="../mysql/templates"
+directory=$1
+
+if [ -z "$directory" ]; then
+  echo "Error: No directory specified."
+  exit 1
+fi
 
 for filename in $(find $directory -name "*.yaml"); do
   echo "Applying manifest: $filename"
